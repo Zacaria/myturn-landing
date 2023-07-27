04336e4072f9a5d9073f04b1adbdb68c99227e2d
Zacaria <>
2023-07-27 16:31:14 +0100

    feat: update data

--- src/shared/data.tsx
+++ src/shared/data.tsx
@@ -472,7 +472,7 @@ export const faqs4Data: FAQsProps = {
 // CallToAction data
 export const callToActionData: CallToActionProps = {
   title: "Now it's Your Turn !",
-  // subtitle: null,
+  subtitle: 'Join our growing community of parents who believe in raising responsible and motivated kids.',
   callToAction: {
     text: 'Get template',
     href: 'https://github.com/onwidget/tailnext',
